b6e3f291843b060d9a484b5c724014ae3a8f6992
cliftonwferguson <clifton.ferguson7@gmail.com>
2018-10-18 13:02:25 -0500

    Form Tabs

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -6,15 +6,16 @@ class RoomList extends Component {
  constructor (props) {
    super(props)
    this.state = {
-     rooms: []
+     rooms: [],
+     newRoom: ''
    };
 
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
     }
-    this.roomsRef.push({
-      name. newRoomName
-    });
+
+    };
+
 
  componentDidMount() {
    this.roomsRef.on('child_added', snapshot => {
@@ -23,11 +24,29 @@ class RoomList extends Component {
      this.setState({ rooms: this.state.rooms.concat( room ) })
    });
  }
+
+   handleChange(event) {
+     this.setState({newRoom: event.target.value});
+
+   }
+
+   newChatRoom() {
+     this.roomsRef.push({
+       name. newRoom
+   }
+
  render () {
    return (
+     <form onSubmit={this.handleSubmit}>
+     <label>
+       Name:
+       <input type="text" value={this.state.value} onChange={this.handleChange} />
+     </label>
+     <input type="submit" value="Submit" />
+   </form>
      <ul>{this.state.rooms.map( (room) => (<li>{room.name}</li>))}</ul>
    )
-     
+     <input type="text" value{this.state.newRoom} onChange={this.handleChange} />
  }
 }
 
