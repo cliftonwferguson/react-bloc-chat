ac93029bf2e2aea43f64c32092cab40356bf7776
cliftonwferguson <clifton.ferguson7@gmail.com>
2018-10-22 12:38:58 -0500

    step back

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,62 +1,60 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from 'react';
 class RoomList extends Component {
 
 
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            rooms: [],
-            newroom: ''
-        };
+
+constructor (props) {
+  super(props)
+  this.state = {
+    rooms: [],
+    newroom: ''
+  };
 
 
-        this.roomsRef = this.props.firebase.database().ref('rooms');
-    }
-
-
-    this.roomsRef.on('child_added', snapshot => {
-        const room = snapshot.val();
-        room.key = snapshot.key;
-
-        this.setState({
-            rooms: this.state.rooms.concat(room)
-        })
-    });
+   this.roomsRef = this.props.firebase.database().ref('rooms');
+   }
 
 
 
-    handleChange(event) {
-        this.setState({
-            newroom: event.target.value
-        });
 
-    }
 
-    newChatRoom(event) {
-        event.preventDefault();
-        this.roomsRef.push({
-            name: this.state.newroom
-        });
-    }
 
-    render () {
-   return (
-    <div>
-     <form onSubmit={(event) => this.newChatRoom(event)}>
-     <label>
-       Name:
-       <input type="text" value={this.state.newroom} onChange={(e) => this.handleChange(e)} />
-     </label>
-     <input type="submit" value="Submit" />
-   </form>
-     <ul>{this.state.rooms.map( room => <li>{room.name}</li>)}</ul>
-    </div>
-   );
+componentDidMount() {
+  this.roomsRef.on('child_added', snapshot => {
+    const room = {name: snapshot.val(), key: snapshot.key}
 
-                    }
+    this.setState({ rooms: this.state.rooms.concat( room ) })
+  });
+}
 
-                  }
-                    export default RoomList;
+  handleChange(event) {
+    this.setState({newroom: event.target.value});
+
+  }
+
+  newChatRoom() {
+    event.preventDefault();
+    this.roomsRef.push({
+      name: this.state.newroom
+  });
+}
+
+render () {
+  return (
+   <div>
+    <form onSubmit={(event) => this.newChatRoom(event)}>
+    <label>
+      Name:
+      <input type="text" value={this.state.newroom} onChange={(e) => this.handleChange(e)} />
+    </label>
+    <input type="submit" value="Submit" />
+  </form>
+    <ul>{this.state.rooms.map( room => <li>{room.name}</li>)}</ul>
+   </div>
+  );
+
+}
+}
+
+export default RoomList;
